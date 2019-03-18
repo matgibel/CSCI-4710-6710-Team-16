e3fe1232b20ba4df1f7a261f66f3753902b91426
Navid Kabir <navid113@gmail.com>
2019-03-18 15:20:35 -0400

    Update fragments Make the upload page using afoster's code

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,32 @@
-const MAX_PAGES = 4;
-let currentPageIndex = 1;
+const PAGES = {
+    'UPLOAD': {
+        'url': 'fragments/upload.htm'
+    },
+    'VERIFY': {
+        'url': 'fragments/verify.htm'
+    },
+    'CONFIGURE': {
+        'url': 'fragments/configure.htm'
+    },
+    'RUN TESTS': {
+        'url': 'fragments/tests.htm'
+    },
+    'REVIEW': {
+        'url': 'fragments/review.htm'
+    },
+};
+
+let currentPageIndex = 0;
 
 function renderBreadcrumbs() {
     let output = "<ol class='breadcrumb'>";
+    let strings = Object.keys(PAGES);
 
-    for (let i = 1; i <= MAX_PAGES; i++) {
+    for (let i = 0; i < strings.length; i++) {
         if (i === currentPageIndex) {
-            output += `<li class='breadcrumb-item active' aria-current='page'>Page ${i}</li>`;
+            output += `<li class='breadcrumb-item active' aria-current='page'>${strings[i]}</li>`;
         } else {
-            output += `<li class='breadcrumb-item'>Page ${i}</li>`;
+            output += `<li class='breadcrumb-item'>${strings[i]}</li>`;
         }
     }
 
@@ -21,11 +39,11 @@ function renderNavButtons() {
     let prevDisabled = "";
     let nextDisabled = "";
 
-    if (currentPageIndex <= 1) {
+    if (currentPageIndex <= 0) {
         prevDisabled = "disabled";
     }
 
-    if (currentPageIndex >= MAX_PAGES) {
+    if (currentPageIndex >= Object.keys(PAGES).length - 1) {
         nextDisabled = "disabled";
     }
 
@@ -36,19 +54,21 @@ function renderNavButtons() {
 }
 
 function loadPage() {
-    $('#page_content').load(`fragments/page${currentPageIndex}_fragment.html`);
+    const key = Object.keys(PAGES)[currentPageIndex];
+    $('#page_content').load(PAGES[key].url);
 }
 
 function previousPage() {
     currentPageIndex--;
-    if (currentPageIndex < 1) currentPageIndex = 1;
+    if (currentPageIndex < 0) currentPageIndex = 0;
 
     refresh();
 }
 
 function nextPage() {
     currentPageIndex++;
-    if (currentPageIndex >= MAX_PAGES) currentPageIndex = MAX_PAGES;
+    const length = Object.keys(PAGES).length;
+    if (currentPageIndex >= length - 1) currentPageIndex = length - 1;
 
     refresh();
 }
