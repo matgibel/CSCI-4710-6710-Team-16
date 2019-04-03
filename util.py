fb53c54038be3f48daef34cbb60d2aa2ac290441
Mathew Robert Gibel <39527719+nachochips18@users.noreply.github.com>
2019-04-03 12:41:59 -0400

    Initial Start
    Hey

---
+++ util.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+# if a file extension is not listed, the system will not upload the file
+ALLOWED_EXTENSIONS = set(['csv'])
+
+def allowed_file(filename):
+    return '.' in filename and \
+filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+
